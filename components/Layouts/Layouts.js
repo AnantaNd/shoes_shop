d750ab09c9e0902e44f8200623469c92d73e5af9
dhanakbar <maulana261100@gmail.com>
2022-10-13 04:25:35 +0700

    Components: Layouts, Navbar

---
+++ components/Layouts/Layouts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+const Layouts = () => {
+    return (
+        <>
+            <Navbar/>
+        </>
+    )
+}
+
+export default Layouts
