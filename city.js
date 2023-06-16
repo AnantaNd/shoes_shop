05e565003cb638f6b63278e6a9a3ccc87fdadb99
AnantaNd <ananta.nov2@gmail.com>
2023-06-16 08:10:43 +0700

    add data city

--- city.js
+++ city.js
@@ -19,4 +19,49 @@ export const city = [
     name: 'cirebon',
     price: 10000
   },
+  {
+    id: 5,
+    name: 'ciamis',
+    price: 10000
+  },
+  {
+    id: 5,
+    name: 'garut',
+    price: 10000
+  },
+  {
+    id: 6,
+    name: 'purwakarta',
+    price: 10000
+  },
+  {
+    id: 7,
+    name: 'sukabumi',
+    price: 10000
+  },
+  {
+    id: 8,
+    name: 'tasikmalaya',
+    price: 10000
+  },
+  {
+    id: 9,
+    name: 'banjar',
+    price: 10000
+  },
+  {
+    id: 10,
+    name: 'bekasi',
+    price: 10000
+  },
+  {
+    id: 11,
+    name: 'cianjur',
+    price: 10000
+  },
+  {
+    id: 12,
+    name: 'indramayu',
+    price: 10000
+  },
 ]
